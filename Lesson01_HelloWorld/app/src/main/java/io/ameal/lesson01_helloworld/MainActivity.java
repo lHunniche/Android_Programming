42decb82932df57058c87cd8b66e0c050c82c953
Lasse <lasse96-06@hotmail.com>
2019-02-20 17:55:01 +0100

    OwO noice

--- Lesson01_HelloWorld/app/src/main/java/io/ameal/lesson01_helloworld/MainActivity.java
+++ Lesson01_HelloWorld/app/src/main/java/io/ameal/lesson01_helloworld/MainActivity.java
@@ -1,15 +1,26 @@
 package io.ameal.lesson01_helloworld;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    private TextView firstTextOwO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initComps();
+    }
+
+    private void initComps()
+    {
+        firstTextOwO = findViewById(R.id.myFirstTextView);
+        firstTextOwO.setText("Please eradicate anime");
+
+        getSupportActionBar().hide();
     }
 }
