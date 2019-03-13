a78be541a4399e37c15399c2c3d4cb8d69664bd9
Lasse <lasse96-06@hotmail.com>
2019-03-13 13:11:32 +0100

    næste projekt kører

---
+++ Lesson04_LocalizationLandscape/app/src/main/java/io/ameal/lesson04_localizationlandscape/MainActivity.java
@@ -0,0 +1,15 @@
+package io.ameal.lesson04_localizationlandscape;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
